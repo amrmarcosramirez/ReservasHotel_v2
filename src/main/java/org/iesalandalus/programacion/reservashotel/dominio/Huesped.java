6255bf59a765816cf814af3802d52541b185902a
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2023-12-22 12:44:26 +0100

    Update Huesped.java
    Se crean los atributos con su visibilidad adecuada

--- src/main/java/org/iesalandalus/programacion/reservashotel/dominio/Huesped.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/dominio/Huesped.java
@@ -1,4 +1,17 @@
 package org.iesalandalus.programacion.reservashotel.dominio;
 
+import java.time.LocalDate;
+
 public class Huesped {
+    // Se crean los atributos con su visibilidad adecuada
+    private static String ER_TELEFONO;
+    private static String ER_CORREO;
+    private static String ER_DNI;
+    public static String FORMATO_FECHA;
+    private String nombre;
+    private String telefono;
+    private String correo;
+    private String dni;
+    private LocalDate fechaNacimiento;
+
 }
