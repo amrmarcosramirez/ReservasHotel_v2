4f49a0d49f3bf2f176b529fad5c17f73b721a063
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2024-01-21 13:59:09 +0100

    Update TipoHabitacion.java
    Se añaden comentarios descriptivos de ayuda

--- src/main/java/org/iesalandalus/programacion/reservashotel/dominio/TipoHabitacion.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/dominio/TipoHabitacion.java
@@ -6,18 +6,22 @@ public enum TipoHabitacion {
     DOBLE("DOBLE", 2),
     TRIPLE("TRIPLE", 3);
 
+    // Se crean los atributos con su visibilidad adecuada
     private String descripcion;
     private int numeroMaximoPersonas;
 
+    //Constructor
     TipoHabitacion(String descripcion, int numeroMaximoPersonas) {
         this.descripcion = descripcion;
         this.numeroMaximoPersonas = numeroMaximoPersonas;
     }
 
+    //Método de acceso
     public int getNumeroMaximoPersonas() {
         return numeroMaximoPersonas;
     }
 
+    //Método toString
     @Override
     public String toString() {
         return String.format("TipoHabitacion[descripción=%s, número personas=%s]",
