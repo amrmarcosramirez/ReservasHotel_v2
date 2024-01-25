05b66229b200e913de1afbf9cf843142a839ae9b
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2024-01-25 20:17:56 +0100

    Create Opcion.java
    Se crea el enumerado Opcion

---
+++ src/main/java/org/iesalandalus/programacion/reservashotel/vista/Opcion.java
@@ -0,0 +1,31 @@
+package org.iesalandalus.programacion.reservashotel.vista;
+
+public enum Opcion {
+    SALIR("Salir"),
+    INSERTAR_HUESPED("Insertar huésped"),
+    BUSCAR_HUESPED("Buscar huésped"),
+    BORRAR_HUESPED("Borrar huésped"),
+    MOSTRAR_HUESPEDES("Mostrar huéspedes"),
+    INSERTAR_HABITACION("Insertar habitación"),
+    BUSCAR_HABITACION("Buscar habitación"),
+    BORRAR_HABITACION("Borrar habitación"),
+    MOSTRAR_HABITACIONES("Mostrar habitaciones"),
+    INSERTAR_RESERVA("Insertar reserva"),
+    ANULAR_RESERVA("Anular reserva"),
+    MOSTRAR_RESERVAS("Mostrar reservas"),
+    CONSULTAR_DISPONIBILIDAD("Consultar disponibilidad");
+
+    // Se crean los atributos con su visibilidad adecuada
+    private String mensajeAMostrar;
+
+    //Constructor
+    Opcion(String mensajeAMostrar) {
+        this.mensajeAMostrar = mensajeAMostrar;
+    }
+
+    //Método toString
+    @Override
+    public String toString() {
+        return String.format("%d.- %s", ordinal(), this.mensajeAMostrar);
+    }
+}
