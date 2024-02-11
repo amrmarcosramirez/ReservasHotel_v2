05e8f3964a21024b087caa7771df3e9129e11486
amrmarcosramirez <150553422+amrmarcosramirez@users.noreply.github.com>
2024-02-11 01:05:55 +0100

    Update Controlador.java
    Se actualiza la clase con ArrayList

--- src/main/java/org/iesalandalus/programacion/reservashotel/controlador/Controlador.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/controlador/Controlador.java
@@ -10,6 +10,7 @@ import org.iesalandalus.programacion.reservashotel.vista.Vista;
 
 import javax.naming.OperationNotSupportedException;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
 
 public class Controlador {
@@ -28,7 +29,7 @@ public class Controlador {
     }
 
     //Se crean los métodos
-    public void comenzar() {
+    public void comenzar() throws OperationNotSupportedException {
         modelo.comenzar();
         vista.comenzar();
     }
@@ -50,7 +51,7 @@ public class Controlador {
         modelo.borrar(huesped);
     }
 
-    public Huesped[] getHuespedes() {
+    public List<Huesped> getHuespedes() {
         return modelo.getHuespedes();
     }
 
@@ -66,11 +67,11 @@ public class Controlador {
         modelo.borrar(habitacion);
     }
 
-    public Habitacion[] getHabitaciones() {
+    public List<Habitacion> getHabitaciones() {
         return modelo.getHabitaciones();
     }
 
-    public Habitacion[] getHabitaciones(TipoHabitacion tipoHabitacion) {
+    public List<Habitacion> getHabitaciones(TipoHabitacion tipoHabitacion) {
         return modelo.getHabitaciones(tipoHabitacion);
     }
 
@@ -86,19 +87,19 @@ public class Controlador {
         modelo.borrar(reserva);
     }
 
-    public Reserva[] getReservas() {
+    public List<Reserva> getReservas() {
         return modelo.getReservas();
     }
 
-    public Reserva[] getReservas(Huesped huesped) {
+    public List<Reserva> getReservas(Huesped huesped) {
         return modelo.getReservas(huesped);
     }
 
-    public Reserva[] getReservas(TipoHabitacion tipoHabitacion) {
+    public List<Reserva> getReservas(TipoHabitacion tipoHabitacion) {
         return modelo.getReservas(tipoHabitacion);
     }
 
-    public Reserva[] getReservaFuturas(Habitacion habitacion) {
+    public List<Reserva> getReservaFuturas(Habitacion habitacion) {
         return modelo.getReservasFuturas(habitacion);
     }
 
